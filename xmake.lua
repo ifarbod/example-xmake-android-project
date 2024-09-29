61811bb0ea6821d3c501a205d3820412937a54d7
iFarbod <ifarbod@users.noreply.github.com>
2024-09-29 18:01:54 +0330

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,14 @@
-set_xmakever("2.8.7")
+set_xmakever("2.9.5")
 
-set_languages("c17", "cxx20")
+set_languages("c17", "cxx14")
 
 add_rules("mode.debug", "mode.releasedbg", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
 add_rules("plugin.compile_commands.autoupdate")
+
+if is_mode("release") then
+    set_optimize("fastest")
+end
 
 target("myapplicationcpp2")
     if is_plat("android") then
@@ -14,5 +18,15 @@ target("myapplicationcpp2")
         set_kind("binary")
     end
 
+    set_symbols("debug")
+    set_strip("all")
+
     add_files("src/**.cpp")
     add_includedirs("src/")
+
+    -- TODO(iFarbod): x64 too ?
+    -- if is_plat("android") and is_arch("arm64-v8a") then
+        -- add_ldflags("-z max-page-size=16384", {force = true})
+        -- add_cxflags("-Wl -z max-page-size=16384", {force = true})
+        add_ldflags("-z max-page-size=16384", {force = true})
+    -- end
